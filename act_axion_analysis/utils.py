173279f582c22044fdcf608f26185836a302101c
Zach Huber <78050507+zhuber21@users.noreply.github.com>
2025-03-28 17:59:58 -0400

    Add skewnorm to utils

--- act_axion_analysis/utils.py
+++ act_axion_analysis/utils.py
@@ -1,8 +1,18 @@
 # Small functions needed in common by multiple packages
 
 import numpy as np
+import scipy
 
 def gaussian(x,mean,sigma):
     """Normalized Gaussian for curve_fit"""
     amp = 1.0
     return amp*np.exp(-(x-mean)**2/(2*sigma**2))
+
+def norm_skewnorm_pdf(x, mu, sigma, alpha):
+    """Normalized skew normal distribution for curve_fit
+       alpha is the skew parameter. 
+       mu and sigma are no longer exactly their Gaussian counterparts.
+       Most notably, the peak is not at mu unless alpha=0.
+    """
+    skewnorm = np.exp(-(x-mu)**2/(2*sigma**2))*(1+scipy.special.erf(alpha*((x-mu)/(sigma*np.sqrt(2)))))
+    return skewnorm/np.max(skewnorm)
